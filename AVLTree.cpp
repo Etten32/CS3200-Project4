f0bb11fd0c95e747a3ef3e1acd8156abf284a430
Etten32 <etten327vcr@gmail.com>
2022-03-22 19:14:27 -0400

    Public documentation finished: Private still in progress

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -16,6 +16,7 @@ AVLTree::AVLTree(AVLTree& original){
     helpMake(original.root, this);
 }    
 
+// Adds all nodes in subtree of AVLTree::helpMake().nodeAt inclusive to AVLTree::helpMake().treeToAdd
 void AVLTree::helpMake(TreeNode* nodeAt, AVLTree* treeToAdd){
         //*base case: if node is null
         if(nodeAt == nullptr) return;
@@ -81,12 +82,11 @@ bool AVLTree::testRotate(int key, TreeNode* nodeAt, char shift){
 
         //*base case 1: if key is found
         if(nodeAt->key == key){
-            if(shift == 'r') rightRotate(nodeAt);
-            else if(shift == 'l') leftRotate(nodeAt);
-            else if(shift == 'R') doubleRightR(nodeAt);
-            else if(shift == 'L') doubleLeftR(nodeAt);
+            if(shift == 'r') return rightRotate(nodeAt);
+            else if(shift == 'l') return leftRotate(nodeAt);
+            else if(shift == 'R') return doubleRightR(nodeAt);
+            else if(shift == 'L') return doubleLeftR(nodeAt);
             else return false;
-            return true;
         }
 
         //*recursive case: call helpFind() on appropriate pointer
@@ -99,7 +99,7 @@ bool AVLTree::rightRotate(TreeNode* toRotate){
     if(toRotate == nullptr || toRotate->left == nullptr) return false;
     
     TreeNode* hook = toRotate->left;
-    TreeNode* clip = hook->right;
+    TreeNode* clip = hook->right;       // temp storage
     // if node to rotate at is the root
     if(toRotate->key == this->root->key){
         if(clip != nullptr) hook->unlinkRight();
@@ -109,14 +109,16 @@ bool AVLTree::rightRotate(TreeNode* toRotate){
         this->root = hook;
     }
     else{
+        // if node to rotate ins't the root
         TreeNode* parent = toRotate->parent;
 
-        if(clip != nullptr) hook->unlinkRight();
+        if(clip != nullptr) hook->unlinkRight(); 
         toRotate->unlinkLeft(); 
         hook->linkRight(toRotate);
         if(clip != nullptr) toRotate->linkLeft(clip);
-        parent->replacePointerWith(toRotate, hook);
+        parent->replacePointerWith(toRotate, hook); // replace old parents left or right * with a * to hook
     }
+    // recalculate changed heights
     toRotate->recalculateHeight();
     hook->recalculateHeight();
     return true;
@@ -159,8 +161,8 @@ bool AVLTree::leftRotate(TreeNode* toRotate){
 bool AVLTree::doubleLeftR(TreeNode* toRotate){
     return rightRotate(toRotate->right) && leftRotate(toRotate);
 } 
-///*
 
+// make one AVLTree equal to the other (this <-equal-to-< toTransfer)
 AVLTree& AVLTree::operator=( const AVLTree& toTransfer ){
     // clear whatever is there first
     this->clearTree(this->root->left);
@@ -168,7 +170,8 @@ AVLTree& AVLTree::operator=( const AVLTree& toTransfer ){
 
     TreeNode* toDelete = this->root;
     this->root = nullptr;
-    
+    this->numElts--;
+
     // copy over from toTransfer
     //AVLTree* copyPtr = toTransfer.root;
     this->makeTree(toTransfer.root);
@@ -177,14 +180,14 @@ AVLTree& AVLTree::operator=( const AVLTree& toTransfer ){
 
     return (*this);
 
-}//*/
+}
 
 // prints out AVLTree
-
 ostream& operator<<(ostream& os, const AVLTree& me){
     return me.helpPrint(os, me.root, 0);
 }
 
+// recursive function to print out AVLTree
 ostream& AVLTree::helpPrint(ostream& os, TreeNode* nodeAt, int level) const{
         //*base case: if node is null
         if(nodeAt == nullptr) return os;
@@ -199,8 +202,8 @@ ostream& AVLTree::helpPrint(ostream& os, TreeNode* nodeAt, int level) const{
         return helpPrint(os, nodeAt->left, level + 1);
     }
 
-
-    bool AVLTree::insertNode(int key, string value, TreeNode*& nodeAt){
+// recursive function to insert a node (includes balancing)
+bool AVLTree::insertNode(int key, string value, TreeNode*& nodeAt){
         //*base case 0: node (and tree) is empty
         if(nodeAt == nullptr){
             nodeAt = new TreeNode(key, value);
@@ -260,8 +263,8 @@ ostream& AVLTree::helpPrint(ostream& os, TreeNode* nodeAt, int level) const{
         return false;
     }
 
-    // recursive function to delete nodes
-    void AVLTree::clearTree(TreeNode* nodeAt){
+// recursive function to delete nodes
+void AVLTree::clearTree(TreeNode* nodeAt){
         //*base case: if node is null
         if(nodeAt == nullptr) return;
 
@@ -271,8 +274,8 @@ ostream& AVLTree::helpPrint(ostream& os, TreeNode* nodeAt, int level) const{
         delete(nodeAt);
     }
 
-    // recursively inserts from a given node into this tree
-    void AVLTree::makeTree(TreeNode* nodeAt){
+// recursively inserts from a given node into this tree
+void AVLTree::makeTree(TreeNode* nodeAt){
         //*base case: if node is null
         if(nodeAt == nullptr) return;
 
@@ -282,8 +285,8 @@ ostream& AVLTree::helpPrint(ostream& os, TreeNode* nodeAt, int level) const{
         this->insert(nodeAt->key, nodeAt->elt);
     }
 
-    // recursive function to help find node
-    bool AVLTree::helpFind(int key, string& value, TreeNode* nodeAt){
+// recursive function to help find node
+bool AVLTree::helpFind(int key, string& value, TreeNode* nodeAt){
         //*base case 0: if node is null
         if(nodeAt == nullptr) return false;
 
@@ -297,5 +300,5 @@ ostream& AVLTree::helpPrint(ostream& os, TreeNode* nodeAt, int level) const{
         if(nodeAt->key < key) return helpFind(key, value, nodeAt->right); // if value in node is less than key (so node to find would be in right subtree)
         else return helpFind(key, value, nodeAt->left); // if value in node is greater than key (so node to find would be in left subtree)
     }     
-	
+
 
--- AVLTree.h
+++ AVLTree.h
@@ -38,6 +38,37 @@
 //  bool AVLTree::insert(int key, string value)
 //      Postcondition: condition-0 = returns false if item error occcured or key was already inserted
 //                     condition-1 = returns true if insert was correct
+//                                   auto-rotates to keep tree balanced
+//
+//  AVLTree& operator=(const AVLTree& toTransfer)
+//      Postcondition: returns a copy of operator=().toTransfer as this *
+//                     deletes previous contents of this *
+//
+//  bool AVLTree::testRotate(int key, char shift)
+//      Postcondition: returns the result of AVLTree::helpFind(key, this->root, shift)
+//                     (see AVLTree::helpFind(int, TreeNode*, char))
+//
+//  bool rightRotate(TreeNode* toRotate)
+//      Postcondition: condition-0 = returns false if prospective AVLTree::rightRotate().toRotate or AVLTree::rightRotate().left is nullptr
+//                     condition-1 = returns true if otherwise, after performing rotation
+//
+//  bool doubleRightR(TreeNode* toRotate)
+//      Postconditon: performs a double right rotation by calling AVLTree::leftRotate() on AVLTree::doubleRightR().toRotate->left
+//                    and AVLTree::rightRotate() on AVLTree::doubleRightR().toRotate
+//                      condition-0 returns false if either rotation call fails
+//                          WARNING : no correction made to return to inputted tree if the second rotation fails after the first succeeding
+//                      condition-1 returns true if both rotations succeed
+//
+//  bool leftRotate(TreeNode* toRotate)
+//      Postcondition: condition-0 = returns false if prospective AVLTree::leftRotate().toRotate or AVLTree::leftRotate().right is nullptr
+//                     condition-1 = returns true if otherwise, after performing rotation
+//
+//  bool doubleLeftRotate(TreeNode* toRotate)
+//      Postconditon: performs a double left rotation by calling AVLTree::rightRotate() on AVLTree::doubleLeftR().toRotate->right
+//                    and AVLTree::leftRotate() on AVLTree::doubleLeftR().toRotate
+//                      condition-0 returns false if either rotation call fails
+//                          WARNING : no correction made to return to inputted tree if the second rotation fails after the first succeeding
+//                      condition-1 returns true if both rotations succeed
 //
 // FREINDS:
 // freind ostream& operator<<(ostream& os, const AVLTree& me)
@@ -50,6 +81,18 @@
 //                                   AVLTree::find().value is left empty
 //                     condition-1 = returns true if key was found in this or any sub-branch and 
 //                                   AVLTree::find().value contains value
+//
+// MUTATION: FUNCTIONS:
+//  bool AVLTree::testRotate(int key, TreeNode* nodeAt, char shift)
+//      Postcondition: condition-0 = returns false if AVLTree::testRotate().shift does not match any valid imput types ('l','L','r','R')
+//                     condition-1 = returns false if AVLTree::testRotate().key is not found in AVLTree::testRotate().nodeAt * subtree inclusive
+//                     condition-2 = returns false if the desired rotation at key failed (see rotation functions)
+//                     condition-3 = returns true if rotation at node AVLTree::testRotate().key was successful
+//      Rotation keys:
+//                      key = 'l' : single left rotation
+//                      key ='L' : double left rotation
+//                      key ='r' : single right rotation
+//                      key = 'R' : double right rotation
 //
 //  void deleteTree(TreeNode* nodeAt)
 //      Postcondition: condition-0 = returns if AVLTree::deleteTree().nodeAt == nullptr
@@ -65,7 +108,7 @@ using namespace std;
 
 class AVLTree {
     private:
-        class TreeNode;
+        class TreeNode;                                             // class of nodes to make up the tree
 
     public:
         AVLTree();                                                  // creates a blank tree
@@ -82,25 +125,25 @@ class AVLTree {
 
         vector<string> findRange(int highKey, int lowKey);          // finds range of keys and puts them into the vector
 
-        bool insert(int key, string value);                                             /** NEEDS: DOCUMENTED: **/
+        bool insert(int key, string value);                         // recursive funcction to insert into an AVLTree and auto-balance
 
-        AVLTree& operator=(const AVLTree& toTransfer);                              /** NEEDS: DOCUMENTED: **/
+        AVLTree& operator=(const AVLTree& toTransfer);              // removes this * contents and assigns toTransfer to this *
 
         friend ostream& operator<<(ostream& os, const AVLTree& me); // prints out AVLTree
 
-        bool testRotate(int key, char shift);                                           /** NEEDS: DOCUMENTED: **/
+        bool testRotate(int key, char shift);                       // goes through and locates a given key to call a rotation on
         
-        bool rightRotate(TreeNode* toRotate);                                            /** NEEDS: DOCUMENTED: **/
+        bool rightRotate(TreeNode* toRotate);                       // makes a single right rotation using the given toRotate node as the problem node                  /** NEEDS: DOCUMENTED: **/
 
-        bool doubleRightR(TreeNode* toRotate);                                           /** NEEDS: DOCUMENTED: **/
+        bool doubleRightR(TreeNode* toRotate);                      // makes a double right rotation using the given toRotate node as the problem node 
 
-        bool leftRotate(TreeNode* toRotate);                                             /** NEEDS: DOCUMENTED: **/
+        bool leftRotate(TreeNode* toRotate);                        // makes a single left rotation using the given toRotate node as the problem node 
 
-        bool doubleLeftR(TreeNode* toRotate);                                            /** NEEDS: DOCUMENTED: **/
+        bool doubleLeftR(TreeNode* toRotate);                       // makes a double left rotation using the given toRotate node as the problem node 
 
     private:
     
-        bool testRotate(int key, TreeNode* nodeAt, char shift);                         /** NEEDS: DOCUMENTED: **/
+        bool testRotate(int key, TreeNode* nodeAt, char shift);     // recursive helper function to test rotate for nodeAt and down (see testRotate(int,char))
 
         void helpMake(TreeNode* nodeAt, AVLTree* treeToAdd);                            /** NEEDS: DOCUMENTED: **/
 
@@ -114,20 +157,17 @@ class AVLTree {
 
         ostream& helpPrint(ostream& os, TreeNode* nodeAt, int level) const;             /** NEEDS: DOCUMENTED: **/
 
-
-
 	    class TreeNode {
 
 
 	    public:
-        /** NEEDS: DOCUMENTED: **/ // vvv ALL VARS BELOW vvv
-		    TreeNode *right;
-		    TreeNode *left;
-            TreeNode *parent;
-            int key;
-		    string elt;
-            int rightHeight;
-            int leftHeight;
+		    TreeNode *right;    // holds the righthand node *
+		    TreeNode *left;     // holds the lefthand node *
+            TreeNode *parent;   // holds the parent node *
+            int key;            // holds the id key for the node
+		    string elt;         // holds the value for the node
+            int rightHeight;    // holds the max height of the right subtree
+            int leftHeight;     // holds the max height of the left subtree
 
 		    TreeNode(int inKey, string inElt) { // defualt constructor          
 			    right = nullptr;
@@ -149,8 +189,9 @@ class AVLTree {
 
             ~TreeNode() {}; // not needed since 'new' is not used
 
-            /** NEEDS: DOCUMENTED: **/
-            // recalculates height
+            //* recalculates height
+            //  void TreeNode::recalculateHeight()
+            //      Postcondition: recalculates both this->rightHeight and this->leftHeight
             void recalculateHeight(){
                 if(this->right == nullptr) rightHeight = 0;
                 else this->rightHeight = this->right->getHeight() + 1; // height is one more than previous
@@ -158,15 +199,18 @@ class AVLTree {
                 else this->leftHeight = this->left->getHeight() + 1; // height is one more than previous
             }
 
-            // get the height of a node in the treee
+            //* get the height of a node in the tree
+            //  int TreeNode::getHeight()
+            //      Postcondition: returns the higher of this->leftHeight and this->rightHeight
             int getHeight(){
                 return this->leftHeight >= this->rightHeight ? this->leftHeight: this->rightHeight;                // return the heigher of the two heights in the root
             //                                                                                                     asistance from this site to format ternary operator: 
             //                                                                                                     https://www.programiz.com/cpp-programming/ternary-operator
             }
 
-            /** NEEDS: DOCUMENTED: **/
-            // calculate and get balance of a node
+            //* calculate and get balance of a node
+            //  int TreeNode::getBalance()
+            //      Postcondition: returns the balance of this node
             int getBalance(){
                 return leftHeight - rightHeight;
             }
@@ -256,9 +300,7 @@ class AVLTree {
 
 	};
     
-        // MEMBER DATA.  These are the data items that each BinarySearchTree object will contain.  Each tree will have a head and numElts
-	    TreeNode* root;
-	    int numElts;   // Number of elements in the sequence
-
-
+        // MEMBER DATA.  These are the data items that each AVLTree object will contain.  Each tree will have a root and numElts
+	    TreeNode* root; // base of tree
+	    int numElts;    // Number of elements in the sequence
 };
